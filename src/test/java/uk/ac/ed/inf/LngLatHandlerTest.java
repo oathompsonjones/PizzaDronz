e41a69289ee61fda01f39d18f40848e2681a170b
Ollie Jones <oathompsonjones@gmail.com>
2023-09-26 16:33:12 +0100

    Update tests

--- src/test/java/uk/ac/ed/inf/LngLatHandlerTest.java
+++ src/test/java/uk/ac/ed/inf/LngLatHandlerTest.java
@@ -7,28 +7,51 @@ import uk.ac.ed.inf.ilp.data.NamedRegion;
 
 public class LngLatHandlerTest extends TestCase {
     LngLatHandler handler = new LngLatHandler();
+
     public void testDistanceTo() {
-        LngLat startPos = new LngLat(0, 4);
-        LngLat endPos = new LngLat(3, 0);
-        assertEquals(5.0, handler.distanceTo(startPos, endPos));
+        LngLat[] positions = new LngLat[] {
+                new LngLat(0, 4),
+                new LngLat(3, 0),
+                new LngLat(0, 0),
+                new LngLat(0, 1),
+                new LngLat(1, 0),
+                new LngLat(1, 1),
+                new LngLat(-1, -1)
+        };
+        assertEquals(5.0, handler.distanceTo(positions[0], positions[1]));
+        assertEquals(5.0, handler.distanceTo(positions[1], positions[0]));
+        assertEquals(0.0, handler.distanceTo(positions[2], positions[2]));
+        assertEquals(1.0, handler.distanceTo(positions[2], positions[3]));
+        assertEquals(1.0, handler.distanceTo(positions[2], positions[4]));
+        assertEquals(Math.sqrt(2), handler.distanceTo(positions[3], positions[4]));
+        assertEquals(Math.sqrt(8), handler.distanceTo(positions[5], positions[6]));
     }
 
     public void testIsCloseTo() {
-        LngLat pos1 = new LngLat(0, 0);
-        LngLat pos2 = new LngLat(0, SystemConstants.DRONE_IS_CLOSE_DISTANCE);
-        LngLat pos3 = new LngLat(SystemConstants.DRONE_IS_CLOSE_DISTANCE, SystemConstants.DRONE_IS_CLOSE_DISTANCE);
-        assertTrue(handler.isCloseTo(pos1, pos2));
-        assertFalse(handler.isCloseTo(pos1, pos3));
+        LngLat[] positions = new LngLat[] {
+                new LngLat(0, 0),
+                new LngLat(0, SystemConstants.DRONE_IS_CLOSE_DISTANCE),
+                new LngLat(SystemConstants.DRONE_IS_CLOSE_DISTANCE, SystemConstants.DRONE_IS_CLOSE_DISTANCE)
+        };
+        assertTrue(handler.isCloseTo(positions[0], positions[0]));
+        assertTrue(handler.isCloseTo(positions[0], positions[1]));
+        assertFalse(handler.isCloseTo(positions[0], positions[2]));
     }
 
     public void testIsInRegion() {
         LngLat[] vertices = new LngLat[] {
                 new LngLat(0, 0),
-                new LngLat(0, 4),
-                new LngLat(4, 2)
+                new LngLat(4, 0),
+                new LngLat(2, 4)
         };
-        NamedRegion region = new NamedRegion("Test Region", vertices);
+        NamedRegion region = new NamedRegion("Triangular Region", vertices);
         assertTrue(handler.isInRegion(new LngLat(2, 2), region));
+        assertTrue(handler.isInRegion(vertices[0], region));
+        // TODO These both fail because of the way that Java defines "insideness".
+        // assertTrue(handler.isInRegion(vertices[1], region));
+        // assertTrue(handler.isInRegion(vertices[2], region));
+        assertTrue(handler.isInRegion(new LngLat(2, 0), region));
+        assertFalse(handler.isInRegion(new LngLat(0, 2), region));
         assertFalse(handler.isInRegion(new LngLat(4, 4), region));
     }
 

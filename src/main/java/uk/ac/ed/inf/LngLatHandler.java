80a410c82c9564995b80a97a80c28ccbed95e225
Ollie Jones <oathompsonjones@gmail.com>
2023-10-04 14:55:36 +0100

    Comments + improvements

--- src/main/java/uk/ac/ed/inf/LngLatHandler.java
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -8,25 +8,64 @@ import uk.ac.ed.inf.ilp.interfaces.LngLatHandling;
 import java.awt.geom.Path2D;
 
 public class LngLatHandler implements LngLatHandling {
+    /**
+     * Calculates the distance between two points
+     * @param startPosition the position to calculate the distance from
+     * @param endPosition the position to calculate the distance to
+     * @return the distance between the two points
+     */
     public double distanceTo(LngLat startPosition, LngLat endPosition) {
+        // Uses the Pythagorean theorem to calculate the distance between two points
         return Math.sqrt(Math.pow(endPosition.lng() - startPosition.lng(), 2) + Math.pow(endPosition.lat() - startPosition.lat(), 2));
     }
 
+    /**
+     * Checks if two points are close to each other
+     * @param startPosition the position to check the distance from
+     * @param otherPosition the position to check the distance to
+     * @return true if the distance between the two points is less than the close distance, false otherwise
+     */
     public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
+        // Checks if the distance between two points is less than the close distance
         return distanceTo(startPosition, otherPosition) <= SystemConstants.DRONE_IS_CLOSE_DISTANCE;
     }
 
+    /**
+     * Checks if a position is in a region
+     * @param position the position to check
+     * @param region the region to check
+     * @return true if the position is in the region, false otherwise
+     */
     public boolean isInRegion(LngLat position, NamedRegion region) {
+        // Creates a polygon from the region's vertices
         LngLat[] vertices = region.vertices();
         Path2D path = new Path2D.Double();
         path.moveTo(vertices[0].lng(), vertices[0].lat());
         for (int i = 1; i < vertices.length; i++)
             path.lineTo(vertices[i].lng(), vertices[i].lat());
         path.closePath();
+        // Determines if the position lies on the edge of the polygon, as the path.contains method does not include all the edges
+        for (int i = 0; i < vertices.length; i++) {
+            LngLat vertex1 = vertices[i];
+            LngLat vertex2 = vertices[(i + 1) % vertices.length];
+            if (distanceTo(position, vertex1) + distanceTo(position, vertex2) == distanceTo(vertex1, vertex2))
+                return true;
+        }
+        // Determines if the position lies within the polygon
         return path.contains(position.lng(), position.lat());
     }
 
+    /**
+     * Calculates the next position of the drone
+     * @param startPosition the position to calculate the next position from
+     * @param angle the angle to calculate the next position at
+     * @return the next position of the drone
+     */
     public LngLat nextPosition(LngLat startPosition, double angle) {
+        // If the angle is 999 the drone is hovering
+        if (angle == 999)
+            return startPosition;
+        // Uses trigonometry to calculate the new position
         double lngMultiplier = Math.cos(Math.toRadians(angle));
         double newLng = startPosition.lng() + lngMultiplier * SystemConstants.DRONE_MOVE_DISTANCE;
         double latMultiplier = Math.sin(Math.toRadians(angle));
--- src/test/java/uk/ac/ed/inf/LngLatHandlerTest.java
+++ src/test/java/uk/ac/ed/inf/LngLatHandlerTest.java
@@ -18,12 +18,19 @@ public class LngLatHandlerTest extends TestCase {
                 new LngLat(1, 1),
                 new LngLat(-1, -1)
         };
+        // Test distance with the hypotenuse of a pythagorean triple
         assertEquals(5.0, handler.distanceTo(positions[0], positions[1]));
+        // Test that the result is the same when the parameters are swapped
         assertEquals(5.0, handler.distanceTo(positions[1], positions[0]));
+        // Test that the result is 0 when the parameters are the same
         assertEquals(0.0, handler.distanceTo(positions[2], positions[2]));
+        // Test distance with a vertical line
         assertEquals(1.0, handler.distanceTo(positions[2], positions[3]));
+        // Test distance with a horizontal line
         assertEquals(1.0, handler.distanceTo(positions[2], positions[4]));
+        // Test distance with a diagonal line
         assertEquals(Math.sqrt(2), handler.distanceTo(positions[3], positions[4]));
+        // Test distance with a longer diagonal line
         assertEquals(Math.sqrt(8), handler.distanceTo(positions[5], positions[6]));
     }
 
@@ -33,44 +40,77 @@ public class LngLatHandlerTest extends TestCase {
                 new LngLat(0, SystemConstants.DRONE_IS_CLOSE_DISTANCE),
                 new LngLat(SystemConstants.DRONE_IS_CLOSE_DISTANCE, SystemConstants.DRONE_IS_CLOSE_DISTANCE)
         };
+        // Test that the result is true when the parameters are the same
         assertTrue(handler.isCloseTo(positions[0], positions[0]));
+        // Test that the result is true when the parameters are close
         assertTrue(handler.isCloseTo(positions[0], positions[1]));
+        // Test that the result is false when the parameters are not close
         assertFalse(handler.isCloseTo(positions[0], positions[2]));
     }
 
     public void testIsInRegion() {
+        // Test a triangular region
         LngLat[] vertices = new LngLat[] {
                 new LngLat(0, 0),
                 new LngLat(4, 0),
                 new LngLat(2, 4)
         };
         NamedRegion region = new NamedRegion("Triangular Region", vertices);
+        // Test a point in the middle(ish)
         assertTrue(handler.isInRegion(new LngLat(2, 2), region));
-        assertTrue(handler.isInRegion(vertices[0], region));
-        // TODO These both fail because of the way that Java defines "insideness".
-        // assertTrue(handler.isInRegion(vertices[1], region));
-        // assertTrue(handler.isInRegion(vertices[2], region));
+        // Test the corners themselves
+        for (LngLat vertex : vertices)
+            assertTrue(handler.isInRegion(vertex, region));
+        // Test a point on the edge
         assertTrue(handler.isInRegion(new LngLat(2, 0), region));
+        // Test points outside the region
         assertFalse(handler.isInRegion(new LngLat(0, 2), region));
         assertFalse(handler.isInRegion(new LngLat(4, 4), region));
+
+        // Test a square region
+        vertices = new LngLat[] {
+                new LngLat(0, 0),
+                new LngLat(4, 0),
+                new LngLat(4, 4),
+                new LngLat(0, 4)
+        };
+        region = new NamedRegion("Square Region", vertices);
+        // Test a point in the middle
+        assertTrue(handler.isInRegion(new LngLat(2, 2), region));
+        // Test the corners themselves
+        for (LngLat vertex : vertices)
+            assertTrue(handler.isInRegion(vertex, region));
+        // Test a point on the edge
+        assertTrue(handler.isInRegion(new LngLat(2, 0), region));
+        // Test a point outside the region
+        assertFalse(handler.isInRegion(new LngLat(5, 5), region));
     }
 
-    public void testNextPosition()  {
+    public void testNextPosition() {
         LngLat startPos = new LngLat(0, 0);
+        // Test that an angle of 0 results in a position to the right
         LngLat nextPos = handler.nextPosition(startPos, 0);
         assertTrue(SystemConstants.DRONE_MOVE_DISTANCE < nextPos.lng() + 1e-12 && SystemConstants.DRONE_MOVE_DISTANCE > nextPos.lng() - 1e-12);
         assertTrue(0.0 < nextPos.lat() + 1e-12 && 0.0 > nextPos.lat() - 1e-12);
 
+        // Test that an angle of 90 results in a position above
         nextPos = handler.nextPosition(startPos, 90);
         assertTrue(0.0 < nextPos.lng() + 1e-12 && 0.0 > nextPos.lng() - 1e-12);
         assertTrue(SystemConstants.DRONE_MOVE_DISTANCE < nextPos.lat() + 1e-12 && SystemConstants.DRONE_MOVE_DISTANCE > nextPos.lat() - 1e-12);
 
+        // Test that an angle of 180 results in a position to the left
         nextPos = handler.nextPosition(startPos, 180);
         assertTrue(-SystemConstants.DRONE_MOVE_DISTANCE < nextPos.lng() + 1e-12 && -SystemConstants.DRONE_MOVE_DISTANCE > nextPos.lng() - 1e-12);
         assertTrue(0.0 < nextPos.lat() + 1e-12 && 0.0 > nextPos.lat() - 1e-12);
 
+        // Test that an angle of 270 results in a position below
         nextPos = handler.nextPosition(startPos, 270);
         assertTrue(0.0 < nextPos.lng() + 1e-12 && 0.0 > nextPos.lng() - 1e-12);
         assertTrue(-SystemConstants.DRONE_MOVE_DISTANCE < nextPos.lat() + 1e-12 && -SystemConstants.DRONE_MOVE_DISTANCE > nextPos.lat() - 1e-12);
+
+        // Test that an angle of 999 results in the same position
+        nextPos = handler.nextPosition(startPos, 999);
+        assertEquals(0.0, nextPos.lng());
+        assertEquals(0.0, nextPos.lat());
     }
 }

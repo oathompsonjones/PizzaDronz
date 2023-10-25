eb19c2d12cf993095d3a91d225c55a72005ad0a9
Ollie Jones <oathompsonjones@gmail.com>
2023-10-25 15:51:04 +0100

    Take in arguments, and pass into PizzaDronz class

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,13 +1,12 @@
 package uk.ac.ed.inf;
 
+import java.time.LocalDate;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        new PizzaDronz(args[1], LocalDate.parse(args[0]));
     }
 }

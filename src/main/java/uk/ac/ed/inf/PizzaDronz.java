aa26fe8e8f68b00f1d7530e7abb2df525691d84a
Ollie Jones <oathompsonjones@gmail.com>
2023-11-01 12:30:55 +0000

    JavaDoc

--- src/main/java/uk/ac/ed/inf/PizzaDronz.java
+++ src/main/java/uk/ac/ed/inf/PizzaDronz.java
@@ -12,13 +12,37 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Main class for the PizzaDronz application.
+ */
 public class PizzaDronz {
+    /**
+     * Creates an instance of the {@link OrderValidator} class.
+     */
     public static final OrderValidator      orderValidator = new OrderValidator();
+    /**
+     * Creates an instance of the {@link LngLatHandler} class.
+     */
     public static final LngLatHandler       lngLatHandler  = new LngLatHandler();
+    /**
+     * Stores the location of the Appleton Tower.
+     */
     public static final LngLat              appletonTower  = new LngLat(-3.186874, 55.944494);
+    /**
+     * Store the instance of the {@link FlightPathGenerator} class.
+     */
     public final        FlightPathGenerator flightPathGenerator;
+    /**
+     * Stores the instance of the {@link RESTManager} class.
+     */
     private final       RESTManager         restManager;
 
+    /**
+     * Creates an instance of the {@link PizzaDronz} class.
+     *
+     * @param apiUrl The URL of the API.
+     * @param date   The date to generate the flight paths for.
+     */
     public PizzaDronz(String apiUrl, LocalDate date) {
         restManager = new RESTManager(apiUrl);
         flightPathGenerator = new FlightPathGenerator(restManager.getCentralArea(), restManager.getNoFlyZones(), restManager.getRestaurants());
@@ -26,10 +50,23 @@ public class PizzaDronz {
         List<List<LngLat>> flightPaths = flightPathGenerator.generate(validOrders);
     }
 
+    /**
+     * Gets the restaurant from the given order.
+     *
+     * @param order       The order to get the restaurant from.
+     * @param restaurants The list of restaurants to search through.
+     * @return The restaurant from the given order.
+     */
     public static Restaurant getOrderRestaurant(Order order, Restaurant[] restaurants) {
         return Arrays.stream(restaurants).filter(r -> Arrays.stream(r.menu()).anyMatch(p -> Objects.equals(p.name(), order.getPizzasInOrder()[0].name()))).toArray(Restaurant[]::new)[0];
     }
 
+    /**
+     * Fetches and validates all orders for the given date.
+     *
+     * @param date The date to fetch the orders for.
+     * @return The valid orders.
+     */
     private Order[] getValidOrders(LocalDate date) {
         Restaurant[] restaurants     = restManager.getRestaurants();
         Order[]      orders          = restManager.getOrders(date);
@@ -39,21 +76,41 @@ public class PizzaDronz {
         return validOrders;
     }
 
+    /**
+     * Generates the deliveries JSON file for the given date.
+     *
+     * @param date The date to generate the JSON files for.
+     */
     private void generateDeliveryJSON(LocalDate date) {
         String fileName = "deliveries-" + date.toString() + ".json";
         writeFile(fileName, "");
     }
 
+    /**
+     * Generates the flight path JSON file for the given date.
+     *
+     * @param date The date to generate the JSON files for.
+     */
     private void generateFlightPathJSON(LocalDate date) {
         String fileName = "flightpath-" + date.toString() + ".json";
         writeFile(fileName, "");
     }
 
+    /**
+     * Generates the flight path GeoJSON file for the given date.
+     *
+     * @param date The date to generate the GeoJSON files for.
+     */
     private void generateFlightPathGeoJSON(LocalDate date) {
         String fileName = "drone-" + date.toString() + ".geojson";
         writeFile(fileName, "");
     }
 
+    /**
+     * Writes the given data to the given file.
+     *
+     * @param name The name of the file to write to.
+     * @param data The data to write to the file.
     private void writeFile(String name, String data) {
         try {
             FileWriter fileWriter = new FileWriter(name);

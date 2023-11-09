43f0ed2f2124a176b2da716d8a7e3eb79f98d419
Ollie Jones <oathompsonjones@gmail.com>
2023-11-09 11:29:27 +0000

    Create RESTManagerTest.java

---
+++ src/test/java/uk/ac/ed/inf/RestService/RESTManagerTest.java
@@ -0,0 +1,46 @@
+package uk.ac.ed.inf.RestService;
+
+import junit.framework.TestCase;
+import uk.ac.ed.inf.ilp.data.Order;
+
+import java.time.LocalDate;
+
+public class RESTManagerTest extends TestCase {
+    RESTManager manager = new RESTManager("https://ilp-rest.azurewebsites.net");
+
+    public void testGetRestaurants() {
+        try {
+            manager.getRestaurants();
+        } catch (Exception err) {
+            fail("Exception thrown");
+        }
+    }
+
+    public void testGetOrders() {
+        Order[] allOrders  = null;
+        Order[] dateOrders = null;
+        try {
+            allOrders = manager.getOrders(null);
+            dateOrders = manager.getOrders(LocalDate.parse("2023-11-15"));
+        } catch (Exception err) {
+            fail("Exception thrown");
+        }
+        assertTrue(allOrders.length > dateOrders.length);
+    }
+
+    public void testGetCentralArea() {
+        try {
+            manager.getCentralArea();
+        } catch (Exception err) {
+            fail("Exception thrown");
+        }
+    }
+
+    public void testGetNoFlyZones() {
+        try {
+            manager.getNoFlyZones();
+        } catch (Exception err) {
+            fail("Exception thrown");
+        }
+    }
+}
